def54ac4dc7f822c97e8e3e354cfb48254973ab1
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-22 17:49:29 +0300

    Food

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -3,5 +3,16 @@ import 'package:flutter/cupertino.dart';
 class Router {
   static Route onGenerateRoute(RouteSettings settings) {
     print('The route is: ${settings.name}');
+    switch (settings.name) {
+      case '/':
+        return Home.route();
+      case Home.routeName:
+        return Home.route();
+      case Location.routeName:
+        return Location.route();
+        break;
+      default:
+        return _errorRoute();
+    }
   }
 }
--- lib/screen/location/location.dart
+++ lib/screen/location/location.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Location extends StatelessWidget {
-  const Location({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
