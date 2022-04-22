87621d8d57b6f739d965d755d78653d2b18635ac
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-22 18:42:43 +0300

    Food

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -5,5 +5,6 @@ ThemeData theme() {
     primaryColor: Color(0xFFFE3C5B),
     primaryColorDark: Color(0xFFFC0028),
     primaryColorLight: Color(0xFFFE9AAA),
+    scaffoldBackgroundColor: Colors.white,
   );
 }
