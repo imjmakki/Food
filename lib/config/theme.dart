f40a7298926db00260c8eca9796fa7c0e29f990d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-22 18:41:49 +0300

    Food

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData theme() {
   return ThemeData(
     primaryColor: Color(0xFFFE3C5B),
+    primaryColorDark: Color(0xFFFC0028),
+    primaryColorLight: Color(0xFFFE9AAA),
   );
 }
